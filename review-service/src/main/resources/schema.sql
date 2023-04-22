0dbf78e32a8e64d1efb5a12403dbd4fdefe6ed41
anupmehta702 <anupmehta702@gmail.com>
2023-04-22 19:57:22 +0530

    Update schema.sql

--- review-service/src/main/resources/schema.sql
+++ review-service/src/main/resources/schema.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS PRODUCT_REVIEW (
     product_id varchar(50) PRIMARY KEY,
-    product_type int NOT NULL,
-    name int NOT NULL
+    average_review_score int NOT NULL,
+    no_of_reviews int NOT NULL
 );
 
 INSERT INTO PRODUCT_REVIEW(product_id,average_review_score,no_of_reviews) VALUES('AB1234',100,4);
